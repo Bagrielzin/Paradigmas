95465ba72d05eeb14a287c1e06354fc56d0ce482
Bagrielzin <89031177+Bagrielzin@users.noreply.github.com>
2024-09-03 20:10:10 -0300

    Add files via upload

--- GoQ1.go
+++ GoQ1.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	var a,b,c float32
+	var a,b,c float64
 	fmt.Scan(&a,&b,&c)
 
 	delta := b*b - 4*a*c
-	
+
 	if a == 0 || delta < 0 {
 		fmt.Println("Impossivel calcular")
 	} else if delta == 0 {
 		r1 := (-b ) / (2 * a)
 		fmt.Printf("R1 = %f\n", r1)
 	} else {
-		r1 := (-b + delta) / (2 * a)
-		r2 := (-b - delta) / (2 * a)
+		r1 := (-b + math.Sqrt(delta)) / (2 * a)
+		r2 := (-b - math.Sqrt(delta)) / (2 * a)
 		fmt.Printf("R1 = %f\n", r1)
 		fmt.Printf("R2 = %f\n", r2)
 	}
-	
+
 }
